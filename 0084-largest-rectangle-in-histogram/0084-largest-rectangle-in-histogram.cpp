a56df08a2555e8e9ea375598ccc49ffad6bc2663
tan2451220024-a11y <tan_2451220024@dau.edu.vn>
2026-05-10 13:33:41 +0700

    Time: 29 ms (29.38%), Space: 81.5 MB (57.02%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+         stack<int> st;
+        int maxArea = 0;
+        int n = heights.size();
+
+        for (int i = 0; i <= n; i++) {
+
+            // thêm cột 0 giả ở cuối
+            int currentHeight = (i == n) ? 0 : heights[i];
+
+            while (!st.empty() && currentHeight < heights[st.top()]) {
+
+                int h = heights[st.top()];
+                st.pop();
+
+                int width;
+
+                if (st.empty()) {
+                    width = i;
+                } else {
+                    width = i - st.top() - 1;
+                }
+
+                maxArea = max(maxArea, h * width);
+            }
+
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+};
