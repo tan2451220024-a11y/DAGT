fe5c91a0053dd9a8b6a74dd0199b4e9bfb12e1c2
tan2451220024-a11y <tan_2451220024@dau.edu.vn>
2026-05-10 13:48:50 +0700

    Time: 0 ms (100%), Space: 8.3 MB (95.43%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        if (n == 0 || s[0] == '0') return 0;
+
+        int prev2 = 1; // dp[i-2]
+        int prev1 = 1; // dp[i-1]
+
+        for (int i = 1; i < n; i++) {
+
+            int current = 0;
+
+            // 1 chữ số
+            if (s[i] != '0') {
+                current += prev1;
+            }
+
+            // 2 chữ số
+            int twoDigit = (s[i - 1] - '0') * 10 + (s[i] - '0');
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                current += prev2;
+            }
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }  
+    
+};
