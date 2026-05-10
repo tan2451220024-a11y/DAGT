be5944735dc7f7a52595d9fe9c96cd4ffb45b500
tan2451220024-a11y <tan_2451220024@dau.edu.vn>
2026-05-10 13:11:00 +0700

    Time: 0 ms (100%), Space: 13.6 MB (41.58%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        while (left <= right) {
+            // Tránh tràn số với (left + right) / 2
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        // Khi không tìm thấy, left chính là vị trí chèn
+        return left;
+    }
+};
