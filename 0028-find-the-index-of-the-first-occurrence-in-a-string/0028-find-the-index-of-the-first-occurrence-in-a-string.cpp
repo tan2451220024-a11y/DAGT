fa7cb6f14919a06b8d308dd3f43bb82e97064430
tan2451220024-a11y <tan_2451220024@dau.edu.vn>
2026-05-10 13:07:51 +0700

    Time: 0 ms (100%), Space: 9 MB (73.79%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        if (m == 0) return 0;
+        
+        for (int i = 0; i <= n - m; i++) {
+            if (haystack.substr(i, m) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}; // Đảm bảo có dấu đóng này
