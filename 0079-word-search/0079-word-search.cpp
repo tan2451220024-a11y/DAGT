588fb95fb206213cc6778b28979352b9396b088a
tan2451220024-a11y <tan_2451220024@dau.edu.vn>
2026-05-10 13:24:25 +0700

    Time: 187 ms (90.22%), Space: 10.5 MB (65.72%) - LeetHub

---
+++ 0079-word-search/0079-word-search.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+
+    int m, n;
+
+    bool dfs(vector<vector<char>>& board,
+             string& word,
+             int i,
+             int j,
+             int index) {
+
+        // Tìm hết word
+        if (index == word.size())
+            return true;
+
+        // Ra ngoài hoặc không khớp
+        if (i < 0 || j < 0 ||
+            i >= m || j >= n ||
+            board[i][j] != word[index]) {
+
+            return false;
+        }
+
+        char temp = board[i][j];
+
+        // Đánh dấu đã dùng
+        board[i][j] = '#';
+
+        bool found =
+            dfs(board, word, i + 1, j, index + 1) ||
+            dfs(board, word, i - 1, j, index + 1) ||
+            dfs(board, word, i, j + 1, index + 1) ||
+            dfs(board, word, i, j - 1, index + 1);
+
+        // Backtrack
+        board[i][j] = temp;
+
+        return found;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+
+        m = board.size();
+        n = board[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};
