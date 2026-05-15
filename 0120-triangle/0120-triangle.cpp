03a805aa5f123e0d38366975cc9fe78f077d62b5
tan2451220024-a11y <tan_2451220024@dau.edu.vn>
2026-05-15 11:57:53 +0700

    Time: 0 ms (100%), Space: 12.6 MB (77.11%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        vector<int> dp = triangle[n - 1]; // hàng cuối
+
+        // duyệt từ dưới lên
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle[i][j] + min(dp[j], dp[j + 1]);
+            }
+        }
+
+        return dp[0];
+    }
+};
